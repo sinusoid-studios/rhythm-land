e65f574ca2b746925f9b754e50bdcd6f77180239
martendo <51766598+martendo@users.noreply.github.com>
2021-07-08 20:00:13 -0400

    Include Skater Dude tiles and tilemap in game

--- code/games/skater-dude.asm
+++ code/games/skater-dude.asm
@@ -3,6 +3,12 @@ INCLUDE "defines.inc"
 SECTION "Skater Dude Game", ROMX
 
 xGameSkaterDude::
+    ; Load tiles
+    ld      de, xGameSkaterDudeTiles
+    ld      hl, $9000
+    ld      bc, xGameSkaterDudeTiles.end - xGameSkaterDudeTiles
+    rst     LCDMemcopy
+    
     ; Start music
     ld      bc, BANK(Inst_SkaterDude)
     ld      de, Inst_SkaterDude
@@ -38,6 +44,14 @@ xGameSkaterDude::
     ld      b, SFX_SKATER_DUDE_JUMP
     call    SFX_Play
     jr      .loop
+
+xGameSkaterDudeTiles:
+    INCBIN "res/skater-dude/skater-dude.2bpp"
+.end::
+
+xGameSkaterDudeMap:
+    INCBIN "res/skater-dude/skater-dude.tilemap"
+.end::
 
 SECTION "Skater Dude Warning Cue", ROMX
 
