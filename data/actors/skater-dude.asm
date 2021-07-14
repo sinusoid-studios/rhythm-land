980c44087c22bf06a7ae46494d5d36db988924a8
martendo <51766598+martendo@users.noreply.github.com>
2021-07-13 22:14:30 -0400

    Add Skater Dude's falling animation

--- data/actors/skater-dude.asm
+++ data/actors/skater-dude.asm
@@ -19,6 +19,21 @@ xActorSkaterDudeAnimation::
     DB 9, 5
     DB 10, 5
     DB ANIMATION_GOTO, 1
+    
+    ; Falling
+    DB 11, 4
+    DB 12, 4
+    DB 13, 4
+    DB 14, 4
+    DB 15, 4
+    DB 16, 4
+    DB 17, 4
+    DB 18, (MUSIC_SKATER_DUDE_SPEED * 4) - (4 * 7)
+    DB 19, MUSIC_SKATER_DUDE_SPEED * 1
+    DB 18, MUSIC_SKATER_DUDE_SPEED * 1
+    DB 19, MUSIC_SKATER_DUDE_SPEED * 1
+    DB 18, MUSIC_SKATER_DUDE_SPEED * 1
+    DB ANIMATION_GOTO, 0
 
 SECTION "Skater Dude Actor Meta-Sprite Data", ROMX
 
@@ -37,6 +52,19 @@ xActorSkaterDudeMetasprites::
     DW .jumping5
     DW .jumping6
     DW .jumping7
+    
+    ; Falling
+    DW .falling1
+    DW .falling2
+    DW .falling3
+    DW .falling4
+    DW .falling5
+    DW .falling6
+    DW .falling7
+    DW .falling8
+    
+    ; Nothing
+    DW .nothing
 
 .skating1
     ; Skater Dude
@@ -181,4 +209,101 @@ xActorSkaterDudeMetasprites::
     DB 30, 0, $7A, OAMF_PAL1
     DB 30, 8, $72, OAMF_PAL1
     DB 30, 16, $7C, OAMF_PAL1
+    DB METASPRITE_END
+
+.falling1
+    ; Skater Dude
+    DB 0, 0, $00, 0
+    DB 0, 8, $02, 0
+    DB 0, 16, $04, 0
+    DB 16, 0, $12, 0
+    DB 16, 8, $14, 0
+    DB 16, 16, $16, 0
+    ; Skateboard
+    DB 24, 8, $5E, OAMF_PAL1
+    DB 24, 16, $60, OAMF_PAL1
+    DB 24, 24, $62, OAMF_PAL1
+    DB METASPRITE_END
+.falling2
+    ; Skater Dude
+    DB 0, 0, $48, 0
+    DB 0, 8, $4A, 0
+    DB 0, 16, $4C, 0
+    DB 16, 0, $4E, 0
+    DB 16, 8, $50, 0
+    DB 16, 16, $52, 0
+    ; Skateboard
+    DB 24, 16, $88, OAMF_PAL1
+    DB 24, 24, $84, OAMF_PAL1
+    DB 24, 32, $8A, OAMF_PAL1
+    DB METASPRITE_END
+.falling3
+    ; Skater Dude
+    DB 0, 0, $48, 0
+    DB 0, 8, $4A, 0
+    DB 0, 16, $4C, 0
+    DB 16, 0, $4E, 0
+    DB 16, 8, $50, 0
+    DB 16, 16, $52, 0
+    ; Skateboard
+    DB 13, 24, $94, OAMF_PAL1
+    DB 9, 31, $96, OAMF_PAL1
+    DB METASPRITE_END
+.falling4
+    ; Skater Dude
+    DB 0, 0, $48, 0
+    DB 0, 8, $4A, 0
+    DB 0, 16, $4C, 0
+    DB 16, 0, $4E, 0
+    DB 16, 8, $50, 0
+    DB 16, 16, $52, 0
+    ; Skateboard
+    DB 19, 31, $94, OAMF_PAL1
+    DB 15, 38, $96, OAMF_PAL1
+    DB METASPRITE_END
+.falling5
+    ; Skater Dude
+    DB 16, 0, $54, 0
+    DB 16, 8, $56, 0
+    DB 16, 16, $58, 0
+    DB 16, 24, $5A, 0
+    DB 16, 32, $5C, 0
+    ; Skateboard
+    DB 11, 49, $94, OAMF_PAL1
+    DB 7, 56, $96, OAMF_PAL1
+    DB METASPRITE_END
+.falling6
+    ; Skater Dude
+    DB 16, 0, $54, 0
+    DB 16, 8, $56, 0
+    DB 16, 16, $58, 0
+    DB 16, 24, $5A, 0
+    DB 16, 32, $5C, 0
+    ; Skateboard
+    DB 19, 51, $76, OAMF_PAL1
+    DB 19, 59, $72, OAMF_PAL1
+    DB 19, 67, $78, OAMF_PAL1
+    DB METASPRITE_END
+.falling7
+    ; Skater Dude
+    DB 16, 0, $54, 0
+    DB 16, 8, $56, 0
+    DB 16, 16, $58, 0
+    DB 16, 24, $5A, 0
+    DB 16, 32, $5C, 0
+    ; Skateboard
+    DB 19, 60, $76, OAMF_PAL1
+    DB 19, 68, $72, OAMF_PAL1
+    DB 19, 76, $78, OAMF_PAL1
+    DB METASPRITE_END
+.falling8
+    ; Skater Dude
+    DB 16, 0, $54, 0
+    DB 16, 8, $56, 0
+    DB 16, 16, $58, 0
+    DB 16, 24, $5A, 0
+    DB 16, 32, $5C, 0
+    DB METASPRITE_END
+
+.nothing
     DB METASPRITE_END
