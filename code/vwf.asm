f4bb7459ad5c03ad3e0c9048e19360bb14e8756b
martendo <51766598+martendo@users.noreply.github.com>
2021-07-27 16:10:22 -0400

    Tweak gb-vwf configuration

--- code/vwf.asm
+++ code/vwf.asm
@@ -23,6 +23,7 @@
 ; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 ; SOFTWARE.
 
+INCLUDE "constants/hardware.inc"
 INCLUDE "constants/vwf.inc"
 INCLUDE "macros/misc.inc"
 
