9de15c2490b0284b36b157c2ba27b042952b9fec
Eat-butt-loser-butt <75635978+Eat-butt-loser-butt@users.noreply.github.com>
2021-07-28 17:15:10 -0400

    metasprites for ship-actor.png

---
+++ data/actors/ship-actor.asm
@@ -0,0 +1,42 @@
+INCLUDE "constants/actors.inc"
+INCLUDE "macros/actors.inc"
+
+SECTION "Battleship Ship Actor Meta-Sprite Data", ROMX
+
+xActorShipActorAnimation::
+    animation Ship
+
+SECTION "Battleship Ship Actor Meta-Sprite Data", ROMX
+
+xActorShipEntityMetasprite::
+    metasprite .shipCel1
+    metasprite .shipCel2
+
+.shipCel1
+    DB 32, 8, $00, 0
+    DB 16, 8, $02, 0
+    DB 1, 16, $04, 0
+    DB 32, 32, $12, 0
+    DB 16, 32, $14, 0
+    DB 1, 24, $16, 0
+    DB 32, 4, $06, 0
+    DB 16, 8, $08, 0
+    DB 1, 16, $0A, 0
+    DB 32, 36, $0C, 0
+    DB 16, 32, $0E, 0
+    DB 1, 24, $10, 0
+    DB METASPRITE_END
+.shipCel2
+    DB 32, 8, $00, 0
+    DB 16, 8, $02, 0
+    DB 1, 16, $04, 0
+    DB 32, 32, $12, 0
+    DB 16, 32, $14, 0
+    DB 1, 24, $16, 0
+    DB 32, 4, $18, 0
+    DB 16, 8, $1A, 0
+    DB 1, 16, $1C, 0
+    DB 32, 36, $1E, 0
+    DB 16, 32, $20, 0
+    DB 1, 24, $22, 0
+    DB METASPRITE_END
