68e3f2909d1850bb3424eaf6de242845c2765a5a
Eat-butt-loser-butt <75635978+Eat-butt-loser-butt@users.noreply.github.com>
2021-07-28 16:10:39 -0400

    Metasprites for the explosion

---
+++ data/actors/explosion.asm
@@ -0,0 +1,37 @@
+INCLUDE "constants/actors.inc"
+INCLUDE "macros/actors.inc"
+
+SECTION "Battleship Explosion Actor Meta-Sprite Data", ROMX
+
+xActorExplosionMetasprites::
+    metasprite .explosionCel1
+    metasprite .explosionCel2
+    metasprite .explosionCel3
+    metasprite .explosionCel4
+    metasprite .explosionCel5
+    metasprite .explosionCel6
+
+.explosionCel1
+    DB 0, 0, $00, 0
+    DB 0, 8, $02, 0
+    DB METASPRITE_END
+.explosionCel2
+    DB 0, 0, $04, 0
+    DB 0, 8, $06, 0
+    DB METASPRITE_END
+.explosionCel3
+    DB 0, 0, $08, 0
+    DB 0, 8, $0A, 0
+    DB METASPRITE_END
+.explosionCel4
+    DB 0, 0, $0C, 0
+    DB 0, 8, $0E, 0
+    DB METASPRITE_END
+.explosionCel5
+    DB 0, 0, $10, 0
+    DB 0, 8, $12, 0
+    DB METASPRITE_END
+.explosionCel6
+    DB 0, 0, $14, 0
+    DB 0, 8, $16, 0
+    DB METASPRITE_END
