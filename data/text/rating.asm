b5ab255c401b0cf27a6498f1bba934744d65d808
martendo <51766598+martendo@users.noreply.github.com>
2021-07-30 19:33:47 -0400

    Add Seagull Serenade's feedback text
    Co-authored-by: Eat-butt-loser-butt <75635978+Eat-butt-loser-butt@users.noreply.github.com>

--- data/text/rating.asm
+++ data/text/rating.asm
@@ -13,6 +13,7 @@ SECTION "Overall Rating Text Table", ROM0
 
 RatingTextTable::
     rating_text SkaterDude
+    rating_text SeagullSerenade
 .end::
 
 SECTION "Skater Dude Game Bad Rating Text", ROMX
@@ -46,3 +47,37 @@ xTextPerfectSkaterDude:
     DB "Unfazed by the heavy traffic!\n\n"
     DB TEXT_DELAY, 40
     DB "Those were some crazy gnarly tricks!<END>"
+
+SECTION "Seagull Serenade Game Bad Rating Text", ROMX
+
+xTextBadSeagullSerenade:
+    DB "My ears hurt a bit after that.\n\n"
+    DB TEXT_DELAY, 40
+    DB "Quite an embarrassing performance.<END>"
+
+SECTION "Seagull Serenade Game OK Rating Text", ROMX
+
+xTextOKSeagullSerenade:
+    DB  "There were a few parts I liked.\n\n"
+    DB TEXT_DELAY, 40
+    DB  "Lots of room for improvement.\n\n"
+    DB TEXT_DELAY, 40
+    DB  "Pretty average overall.<END>"
+
+SECTION "Seagull Serenade Game Great Rating Text", ROMX
+
+xTextGreatSeagullSerenade:
+    DB "How melodious!\n\n"
+    DB TEXT_DELAY, 40
+    DB "And all while staying in tune, that takes skill.\n\n"
+    DB TEXT_DELAY, 40
+    DB "I'm impressed with that performance!<END>"
+
+SECTION "Seagull Serenade Game Perfect Rating Text", ROMX
+
+xTextPerfectSeagullSerenade:
+    DB "Outstanding!\n\n"
+    DB TEXT_DELAY, 40
+    DB "That may be one of the best songs I've ever heard!\n\n"
+    DB TEXT_DELAY, 40
+    DB "A fantastic showing. Encore!<END>"
