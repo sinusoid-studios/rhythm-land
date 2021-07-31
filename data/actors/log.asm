5c9806402e35f40ba66ac5c872b136eea61f6b46
martendo <51766598+martendo@users.noreply.github.com>
2021-07-30 20:03:03 -0400

    Adjust Skater Dude obstacle positioning

--- data/actors/log.asm
+++ data/actors/log.asm
@@ -23,29 +23,29 @@ xActorLogMetasprites::
     metasprite .log4
 
 .log1
-    obj 16, 0, $BA, OAMF_PAL1
-    obj 8, 8, $BC, OAMF_PAL1
-    obj 8, 16, $BE, OAMF_PAL1
-    obj 24, 8, $D2, OAMF_PAL1
+    obj 16, 8, $BA, OAMF_PAL1
+    obj 8, 16, $BC, OAMF_PAL1
+    obj 8, 24, $BE, OAMF_PAL1
+    obj 24, 16, $D2, OAMF_PAL1
     DB METASPRITE_END
 
 .log2
-    obj 16, 0, $C0, OAMF_PAL1
-    obj 8, 8, $C2, OAMF_PAL1
-    obj 8, 16, $C4, OAMF_PAL1
-    obj 24, 8, $D4, OAMF_PAL1
+    obj 16, 8, $C0, OAMF_PAL1
+    obj 8, 16, $C2, OAMF_PAL1
+    obj 8, 24, $C4, OAMF_PAL1
+    obj 24, 16, $D4, OAMF_PAL1
     DB METASPRITE_END
 
 .log3
-    obj 16, 0, $C6, OAMF_PAL1
-    obj 8, 8, $C8, OAMF_PAL1
-    obj 8, 16, $CA, OAMF_PAL1
-    obj 24, 8, $D6, OAMF_PAL1
+    obj 16, 8, $C6, OAMF_PAL1
+    obj 8, 16, $C8, OAMF_PAL1
+    obj 8, 24, $CA, OAMF_PAL1
+    obj 24, 16, $D6, OAMF_PAL1
     DB METASPRITE_END
 
 .log4
-    obj 16, 0, $CC, OAMF_PAL1
-    obj 8, 8, $CE, OAMF_PAL1
-    obj 8, 16, $D0, OAMF_PAL1
-    obj 24, 8, $D8, OAMF_PAL1
+    obj 16, 8, $CC, OAMF_PAL1
+    obj 8, 16, $CE, OAMF_PAL1
+    obj 8, 24, $D0, OAMF_PAL1
+    obj 24, 16, $D8, OAMF_PAL1
     DB METASPRITE_END
--- data/actors/oil-barrel.asm
+++ data/actors/oil-barrel.asm
@@ -23,33 +23,33 @@ xActorOilBarrelMetasprites::
     metasprite .oilBarrel4
 
 .oilBarrel1
-    obj 8, 0, $DA, OAMF_PAL1
-    obj 8, 8, $DC, OAMF_PAL1
-    obj 8, 16, $DE, OAMF_PAL1
-    obj 24, 0, $EC, OAMF_PAL1
-    obj 24, 8, $EE, OAMF_PAL1
+    obj 8, 8, $DA, OAMF_PAL1
+    obj 8, 16, $DC, OAMF_PAL1
+    obj 8, 24, $DE, OAMF_PAL1
+    obj 24, 8, $EC, OAMF_PAL1
+    obj 24, 16, $EE, OAMF_PAL1
     DB METASPRITE_END
 
 .oilBarrel2
-    obj 8, 0, $E0, OAMF_PAL1
-    obj 8, 8, $E2, OAMF_PAL1
-    obj 8, 16, $E4, OAMF_PAL1
-    obj 24, 0, $F0, OAMF_PAL1
-    obj 24, 8, $EE, OAMF_PAL1
+    obj 8, 8, $E0, OAMF_PAL1
+    obj 8, 16, $E2, OAMF_PAL1
+    obj 8, 24, $E4, OAMF_PAL1
+    obj 24, 8, $F0, OAMF_PAL1
+    obj 24, 16, $EE, OAMF_PAL1
     DB METASPRITE_END
 
 .oilBarrel3
-    obj 8, 0, $E6, OAMF_PAL1
-    obj 8, 8, $E8, OAMF_PAL1
-    obj 8, 16, $E4, OAMF_PAL1
-    obj 24, 0, $F2, OAMF_PAL1
-    obj 24, 8, $EE, OAMF_PAL1
+    obj 8, 8, $E6, OAMF_PAL1
+    obj 8, 16, $E8, OAMF_PAL1
+    obj 8, 24, $E4, OAMF_PAL1
+    obj 24, 8, $F2, OAMF_PAL1
+    obj 24, 16, $EE, OAMF_PAL1
     DB METASPRITE_END
 
 .oilBarrel4
-    obj 8, 0, $E0, OAMF_PAL1
-    obj 8, 8, $EA, OAMF_PAL1
-    obj 8, 16, $DE, OAMF_PAL1
-    obj 24, 0, $F4, OAMF_PAL1
-    obj 24, 8, $EE, OAMF_PAL1
+    obj 8, 8, $E0, OAMF_PAL1
+    obj 8, 16, $EA, OAMF_PAL1
+    obj 8, 24, $DE, OAMF_PAL1
+    obj 24, 8, $F4, OAMF_PAL1
+    obj 24, 16, $EE, OAMF_PAL1
     DB METASPRITE_END
