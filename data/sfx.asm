22384fcffb026be1e0866a7c8982ba36007f4d20
martendo <51766598+martendo@users.noreply.github.com>
2021-08-05 19:18:43 -0400

    Remove test sound effects

--- data/sfx.asm
+++ data/sfx.asm
@@ -3,31 +3,12 @@
 SECTION "Sound Effect Data", ROMX
 
 SFX_Table::
-    DW 0, 0, SFX1_CHNL2, 0
-    DW 0, 0, SFX2_CHNL2, 0
-    DW SFX3_CHNL0, 0, 0, SFX3_CHNL3
-    DW SFX4_CHNL0, 0, 0, 0
-    DW SFX5_CHNL0, 0, 0, 0
-    DW 0, SFX6_CHNL1, 0, 0
+    DW SFX1_CHNL0, 0, 0, SFX1_CHNL3
+    DW SFX2_CHNL0, 0, 0, 0
+    DW SFX3_CHNL0, 0, 0, 0
+    DW 0, SFX4_CHNL1, 0, 0
 
-SFX1_CHNL2:
-    DB $07, $05, $20
-    DB $0A, $00, $01, $23
-    DB $45, $67, $89, $AB
-    DB $CD, $EF, $01, $23
-    DB $45, $67, $89, $AB
-    DB $CD, $EF, $04, $04
-    DB $87, $00, $02, $05
-    DB $40, $00, $01, $02
-SFX2_CHNL2:
-    DB $07, $05, $20
-    DB $0A, $01, $01, $23
-    DB $45, $67, $89, $AB
-    DB $CD, $EF, $FE, $DC
-    DB $BA, $98, $76, $54
-    DB $32, $10, $04, $C1
-    DB $87, $00, $03, $02
-SFX3_CHNL0:
+SFX1_CHNL0:
     DB $07, $0B, $1F
     DB $0A, $00, $05, $F3
     DB $04, $7E, $84, $00
@@ -50,7 +31,7 @@ SFX3_CHNL0:
     DB $00, $01, $0A, $40
     DB $00, $01, $05, $00
     DB $01, $80, $02
-SFX3_CHNL3:
+SFX1_CHNL3:
     DB $07, $0A, $5C
     DB $05, $F2, $01, $80
     DB $00, $01, $0A, $04
@@ -65,7 +46,7 @@ SFX3_CHNL3:
     DB $01, $80, $00, $01
     DB $05, $00, $01, $80
     DB $02
-SFX4_CHNL0:
+SFX2_CHNL0:
     DB $07, $0B, $17
     DB $0A, $00, $05, $F0
     DB $04, $3F, $86, $00
@@ -77,7 +58,7 @@ SFX4_CHNL0:
     DB $0B, $1D, $04, $B0
     DB $86, $00, $06, $05
     DB $00, $01, $80, $02
-SFX5_CHNL0:
+SFX3_CHNL0:
     DB $07, $0B, $17
     DB $0A, $00, $05, $F0
     DB $04, $1F, $87, $00
@@ -88,7 +69,7 @@ SFX5_CHNL0:
     DB $1C, $04, $1F, $87
     DB $00, $08, $05, $00
     DB $01, $80, $02
-SFX6_CHNL1:
+SFX4_CHNL1:
     DB $07, $0A, $40
     DB $05, $80, $04, $43
     DB $87, $00, $01, $05
