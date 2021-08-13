e7ca00ca0a938a98c73543b09f88f302ff7e8741
martendo <51766598+martendo@users.noreply.github.com>
2021-08-13 12:21:38 -0400

    Add cue constants

--- data/cues.asm
+++ data/cues.asm
@@ -3,7 +3,7 @@ INCLUDE "macros/misc.inc"
 SECTION "Cue Subroutine Table", ROM0
 
 CueRoutineTable::
-    full_pointer xCueDangerAlert
-    full_pointer xCueObstacle
-    full_pointer xCueSloMo
+    full_pointer xCueDangerAlert    ; CUE_DANGER_ALERT
+    full_pointer xCueObstacle   ; CUE_OBSTACLE
+    full_pointer xCueSloMo  ; CUE_SLO_MO
 .end::
