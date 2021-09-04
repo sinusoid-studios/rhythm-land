0124d149ff363688dc192b8e4cd365da1281f6b7
martendo <51766598+martendo@users.noreply.github.com>
2021-09-03 21:20:52 -0400

    Move rating text into a single file

--- data/text/feedback.asm
+++ data/text/feedback.asm
@@ -1,3 +1,5 @@
+INCLUDE "constants/charmap.inc"
+INCLUDE "macros/feedback.inc"
 INCLUDE "macros/misc.inc"
 
 ; Simplify making pointers for every type of rating
@@ -14,3 +16,61 @@ RatingTextTable::
     rating_text SkaterDude
     rating_text SeagullSerenade
 .end::
+
+; Skater Dude
+
+SECTION "Skater Dude Game Bad Rating Text", ROMX
+
+xTextBadSkaterDude:
+    feedback "Sheesh, that kid really shouldn't put himself in such danger."
+    feedback_last "Ridiculously irresponsible."
+
+SECTION "Skater Dude Game OK Rating Text", ROMX
+
+xTextOKSkaterDude:
+    feedback "Hmm."
+    feedback "That looked really dangerous, but he had some cool tricks there."
+    feedback_last "It was.<DELAY>",6,".<DELAY>",6,"."
+    ; "OK" appears next, in the rating graphic
+
+SECTION "Skater Dude Game Great Rating Text", ROMX
+
+xTextGreatSkaterDude:
+    feedback "Huh, that dude knows his stuff!"
+    feedback_last "Those tricks were really cool!"
+
+SECTION "Skater Dude Game Perfect Rating Text", ROMX
+
+xTextPerfectSkaterDude:
+    feedback "Woah, that dude was super cool!"
+    feedback "Unfazed by the heavy traffic!"
+    feedback_last "Those were some crazy gnarly tricks!"
+
+; Seagull Serenade
+
+SECTION "Seagull Serenade Game Bad Rating Text", ROMX
+
+xTextBadSeagullSerenade:
+    feedback "My ears hurt a bit after that."
+    feedback_last "Quite an embarrassing performance."
+
+SECTION "Seagull Serenade Game OK Rating Text", ROMX
+
+xTextOKSeagullSerenade:
+    feedback  "There were a few parts I liked."
+    feedback  "Lots of room for improvement."
+    feedback_last  "Pretty average overall."
+
+SECTION "Seagull Serenade Game Great Rating Text", ROMX
+
+xTextGreatSeagullSerenade:
+    feedback "How melodious!"
+    feedback "And all while staying in tune, that takes skill."
+    feedback_last "I'm impressed with that performance!"
+
+SECTION "Seagull Serenade Game Perfect Rating Text", ROMX
+
+xTextPerfectSeagullSerenade:
+    feedback "Outstanding!"
+    feedback "That may be one of the best songs I've ever heard!"
+    feedback_last "A fantastic showing. Encore!"
