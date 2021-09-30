70f63ec21c1b56c9b85a3dc0b8eb0b8b17cef8ae
martendo <51766598+martendo@users.noreply.github.com>
2021-09-30 17:13:31 -0400

    Add the rest of Battleship game's hits and cues

--- data/hits/battleship.asm
+++ data/hits/battleship.asm
@@ -12,6 +12,7 @@ xHitTableBattleship::
     ; Any direction works for the D-Pad side, all combined into Left
     DB PADF_LEFT | PADF_A
     
+    ; Section A
     hit 18, PADF_LEFT
     hit 4, PADF_A
     hit 4, PADF_LEFT
@@ -47,5 +48,85 @@ xHitTableBattleship::
     hit 4, PADF_A
     hit 4, PADF_LEFT | PADF_A
     hit 4, PADF_LEFT | PADF_A
+    
+    ; Section B
+    REPT 2
+    hit 4, PADF_LEFT
+    hit 4, PADF_A
+    hit 4, PADF_LEFT
+    hit 2, PADF_A
+    hit 2, PADF_LEFT
+    hit 4, PADF_A
+    hit 2, PADF_LEFT
+    hit 2, PADF_A
+    hit 4, PADF_LEFT | PADF_A
+    hit 4, PADF_LEFT | PADF_A
+    
+    hit 4, PADF_LEFT
+    hit 4, PADF_LEFT
+    hit 4, PADF_A
+    hit 4, PADF_A
+    hit 4, PADF_LEFT | PADF_A
+    hit 4, PADF_LEFT | PADF_A
+    hit 4, PADF_LEFT
+    hit 2, PADF_A
+    hit 2, PADF_LEFT | PADF_A
+    ENDR
+    
+    ; Section C
+    hit 18, PADF_LEFT | PADF_A
+    hit 4, PADF_LEFT | PADF_A
+    hit 4, PADF_LEFT
+    hit 2, PADF_A
+    hit 2, PADF_LEFT
+    hit 4, PADF_LEFT | PADF_A
+    hit 4, PADF_LEFT | PADF_A
+    hit 4, PADF_A
+    hit 2, PADF_LEFT
+    hit 2, PADF_A
+    hit 4, PADF_LEFT
+    hit 2, PADF_A
+    hit 2, PADF_LEFT
+    hit 2, PADF_A
+    hit 2, PADF_LEFT
+    hit 2, PADF_A
+    hit 2, PADF_LEFT
+    
+    hit 4, PADF_A
+    hit 2, PADF_LEFT
+    hit 2, PADF_A
+    hit 4, PADF_LEFT
+    hit 2, PADF_A
+    hit 2, PADF_LEFT
+    
+    hit 4, PADF_LEFT | PADF_A
+    hit 4, PADF_LEFT | PADF_A
+    hit 4, PADF_LEFT
+    hit 2, PADF_A
+    hit 2, PADF_LEFT
+    hit 4, PADF_LEFT | PADF_A
+    hit 4, PADF_LEFT | PADF_A
+    hit 4, PADF_A
+    hit 2, PADF_LEFT
+    hit 2, PADF_A
+    hit 4, PADF_LEFT
+    hit 2, PADF_A
+    hit 2, PADF_LEFT
+    hit 2, PADF_A
+    hit 2, PADF_LEFT
+    hit 2, PADF_A
+    hit 2, PADF_LEFT
+    
+    hit 4, PADF_A
+    hit 2, PADF_LEFT
+    hit 2, PADF_A
+    hit 4, PADF_LEFT
+    hit 2, PADF_A
+    hit 2, PADF_LEFT
+    
+    ; End
+    hit 4, PADF_LEFT
+    hit 3, PADF_A
+    hit 3, PADF_LEFT | PADF_A
     
     DB HITS_END
